71a4d2e3acc414b4471be9aa9250731f74860882
Sykander <sgul@spartaglobal.com>
2018-05-28 01:25:49 +0100

    improved functionality of the create post option

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-
+    @post.user_id = current_user.id
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
